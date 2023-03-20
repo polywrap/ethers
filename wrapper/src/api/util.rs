42b7e1d6ef7d05d0d78b406bef0f353b30a7adb1
Cesar <cesarbrazon10@gmail.com>
2023-03-20 18:50:23 +0100

    chore(wrapper): add unit tests of to_eth function

--- wrapper/src/api/util.rs
+++ wrapper/src/api/util.rs
@@ -17,5 +17,37 @@ pub fn to_eth(wei: String) -> String {
     };
 
     let parsed_eth = format_units(wei, "ether").unwrap();
-    parsed_eth.trim_end_matches("0").trim_end_matches(".").to_string()
+
+    if parsed_eth.contains(".") {
+        parsed_eth.trim_end_matches("0").trim_end_matches(".").to_string()
+    } else {
+        parsed_eth
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::to_eth;
+    
+    #[test]
+    fn parse_to_eth_integer() {
+        assert_eq!(to_eth("1000000000000000000".to_string()), "1".to_string())
+    }
+
+    #[test]
+    #[should_panic = "Invalid Wei number: 10000000000.00000000"]
+    fn parse_to_eth_not_accepted_decimal() {
+        to_eth("10000000000.00000000".to_string());
+    }
+
+    #[test]
+    fn parse_to_decimal() {
+        assert_eq!(to_eth("1000000000000000".to_string()), "0.001".to_string())
+    }
+
+    #[test]
+    fn parse_to_decimal_with_zeros_in_between() {
+        assert_eq!(to_eth("1004000000000000".to_string()), "0.001004".to_string())
+    }
 }
