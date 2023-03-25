aa67f0d8160a970f38541c70dd172122f43d4d7b
Cesar <cesarbrazon10@gmail.com>
2023-03-25 02:11:43 +0100

    feat: send transaction works!

--- provider/implementations/swift/Sources/MetamaskProvider/MetamaskProvider.swift
+++ provider/implementations/swift/Sources/MetamaskProvider/MetamaskProvider.swift
@@ -1,6 +1,50 @@
 import metamask_ios_sdk
 import Foundation
 import Combine
+import RLPSwift
+
+public struct Transaction: CodableData {
+    let to: String
+    let from: String
+    let value: String
+    let data: String?
+
+    public init(to: String, from: String, value: String, data: String? = nil) {
+        self.to = to
+        self.from = from
+        self.value = value
+        self.data = data
+    }
+    
+    public init?(json: [String: Any]) {
+        guard let to = json["to"] as? String,
+              let from = json["from"] as? String,
+              let value = json["value"] as? String
+        else {
+            return nil
+        }
+        
+        
+        self.to = to
+        self.from = from
+        self.value = value
+        
+        if let data = json["data"] as? String {
+            self.data = data
+        } else {
+            self.data = nil
+        }
+    }
+
+    public func socketRepresentation() -> NetworkData {
+        [
+            "to": to,
+            "from": from,
+            "value": value,
+            "data": data
+        ]
+    }
+}
 
 public struct ArgsRequest {
     var method: String;
@@ -25,17 +69,18 @@ public struct ArgsSignMessage {
 }
 
 public struct ArgsSignTransaction {
-    var message: [UInt8]
+    var rlp: [UInt8]
     var connection: Any?;
     
-    public init(message: [UInt8]) {
-        self.message = message
+    public init(rlp: [UInt8]) {
+        self.rlp = rlp
         self.connection = nil
     }
 }
 
 enum ProviderError: Error {
     case NotConnected
+    case EncodeError
 }
 
 public class MetamaskProvider {
@@ -55,29 +100,51 @@ public class MetamaskProvider {
             print("Wallet connected! \(value)")
         }).store(in: &cancellables)
     }
-    
-    func request(args: ArgsRequest, completion: @escaping (Result<String, Error>) -> Void) {
+        
+    func request(args: ArgsRequest, completion: @escaping (Result<Data, Error>) -> Void) {
         if !provider.connected {
             completion(.failure(ProviderError.NotConnected))
             return
         }
         
-        let params = try! JSONDecoder().decode([String].self, from: args.params)
-        let request = EthereumRequest(method: args.method, params: params)
+        if (args.method == "eth_sendTransaction") {
+            let params: [Transaction] = try! JSONDecoder().decode(
+                [Transaction].self,
+                from: args.params
+            )
 
-        provider.request(request)?.sink(receiveCompletion: { completionResult in
-            switch completionResult {
-            case .finished:
-                break
-            case let .failure(error):
-                completion(.failure(error))
-            }
-        }, receiveValue: { value in
-            completion(.success(value as! String))
-        }).store(in: &cancellables)
+            let request = EthereumRequest(method: args.method, params: params)
+            provider.request(request)?.sink(receiveCompletion: { completionResult in
+                switch completionResult {
+                case .finished:
+                    break
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }, receiveValue: { value in
+                let response = (value as! String).data(using: .utf8)!
+                completion(.success(response))
+            }).store(in: &cancellables)
+
+        } else {
+            let params: [String] = try! JSONDecoder().decode([String].self, from: args.params)
+            let request = EthereumRequest(method: args.method, params: params)
+
+            provider.request(request)?.sink(receiveCompletion: { completionResult in
+                switch completionResult {
+                case .finished:
+                    break
+                case let .failure(error):
+                    completion(.failure(error))
+                }
+            }, receiveValue: { value in
+                let response = (value as! String).data(using: .utf8)!
+                completion(.success(response))
+            }).store(in: &cancellables)
+        }
     }
     
-    public func request(_ args: ArgsRequest) async throws -> String {
+    public func request(_ args: ArgsRequest) async throws -> Data {
         return try await withCheckedThrowingContinuation { continuation in
             request(args: args) { result in
                 switch result {
@@ -94,12 +161,13 @@ public class MetamaskProvider {
         
     }
     
-    public func signTransaction() {
-
+    public func signTransaction(_ args: ArgsSignTransaction) async throws -> String {
+//        throw Error("")
+        return ""
     }
 
-    public func signMessage(_ message: String) async throws -> String {
-        let messageData: Data = Data(message.utf8)
+    public func signMessage(_ args: ArgsSignMessage) async throws -> String {
+        let messageData = Data(args.message)
         let messageHex = "0x" + messageData.hexString
         let address = self.provider.selectedAddress.lowercased()
 
@@ -108,8 +176,9 @@ public class MetamaskProvider {
             method: "personal_sign",
             params: jsonParams
         )
-
-        return try await self.request(request)
+        
+        let response = try await self.request(request)
+        return String(data: response, encoding: .utf8)!
     }
     
     public func address() -> String {
