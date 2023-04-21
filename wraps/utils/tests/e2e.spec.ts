11a9cbe0e41f0714221e63b2b603d9e9bd122ada
krisbitney <kristoferbitney@hotmail.com>
2023-04-21 12:45:15 +0000

    added test case for solidityPack

--- wraps/utils/tests/e2e.spec.ts
+++ wraps/utils/tests/e2e.spec.ts
@@ -1,6 +1,7 @@
 import { ClientConfigBuilder, PolywrapClient } from "@polywrap/client-js";
 import { keccak256 } from "js-sha3";
 import * as path from "path";
+import {ethers} from "ethers";
 
 jest.setTimeout(360000);
 
@@ -250,6 +251,50 @@ describe("Ethereum Wrapper", () => {
         });
         if (!response.ok) throw response.error;
         expect(response.value).toEqual("0x8ba1f109551bd432803012645ac136ddd64dba72000000000000000000000000000000000000000000000000000000000000002d");
+      });
+
+      it("should encode packed ethers", async () => {
+        const types: string[] = [
+          "string", "address", "bool",
+          "uint8", "uint16", "uint32", "uint64", "uint128", "uint256", "uint",
+          "int8", "int16", "int32", "int64", "int128", "int256", "int",
+          "bytes", "bytes8", "bytes16", "bytes24", "bytes32"
+        ];
+        const exampleValues: Record<string, string> = {
+          "string": "hello world",
+          "address": "0x742d35Cc6634C0532925a3b844Bc454e4438f44e",
+          "bool": "true",
+          "uint8": "255",
+          "uint16": "65535",
+          "uint32": "4294967295",
+          "uint64": "18446744073709551615",
+          "uint128": "340282366920938463463374607431768211455",
+          "uint256": "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+          "uint": "115792089237316195423570985008687907853269984665640564039457584007913129639935",
+          "int8": "127",
+          "int16": "32767",
+          "int32": "2147483647",
+          "int64": "9223372036854775807",
+          "int128": "170141183460469231731687303715884105727",
+          "int256": "57896044618658097711785492504343953926634992332820282019728792003956564819967",
+          "int": "57896044618658097711785492504343953926634992332820282019728792003956564819967",
+          "bytes": "0x1234abcd",
+          "bytes8": "0x1234567890abcdef",
+          "bytes16": "0x1234567890abcdef1234567890abcdef",
+          "bytes24": "0x1234567890abcdef1234567890abcdef1234567890abcdef",
+          "bytes32": "0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef"
+        };
+        const values: string[] = Object.values(exampleValues);
+
+        const response = await client.invoke<string>({
+          uri,
+          method: "solidityPack",
+          args: { types, values },
+        });
+        if (!response.ok) throw response.error;
+
+        const expected = ethers.utils.solidityPack(types, values);
+        expect(response.value).toEqual(expected);
       });
     });
   });
